0ea521d38bf19a4a5caff5975f2f4d56bfd56fa9
Sabid Barahona <sabid.barahona@gmail.com>
2023-04-22 22:59:16 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import { Button } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
 function App() {
   return (
     <div className="App">
@@ -11,14 +17,16 @@ function App() {
         <p>
           <Button variation="primary">Hello world</Button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+
       </header>
     </div>
   );
